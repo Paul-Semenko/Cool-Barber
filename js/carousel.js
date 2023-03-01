6a2f921482f84342047b2bfcf62f3049c59a2f0f
Paul-Semenko <paul.semenko.ps@gmail.com>
2023-03-01 16:44:14 +0200

    testimonials-carousel

---
+++ js/carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    items: 4,
+    loop: false,
+    margin: 20,
+    responsiveClass: true,
+});
